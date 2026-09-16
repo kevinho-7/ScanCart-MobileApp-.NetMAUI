a5c4d680ffda9183c111910a26534926619e374f
kevinho-7 <kevingarciaf.kf@outlook.com>
2026-09-16 20:32:03 -0300

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace ScanCart
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "ScanCart" };
+        }
+    }
+}
